c7b4cb965327252d1dff6a232b5f8734a68f9f54
Siddhi Kale <siddhikale1998@gmail.com>
2023-07-20 11:08:47 -1100

    commit for edit and update users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ def new
 @user = User.new
 end
 
+def edit
+@user = User.find(params[:id])
+end
+
 def create
 @user = User.new(params.require(:user).permit(:username, :email, :password))
 if @user.save
@@ -12,4 +16,15 @@ else
     render :new, status: :unprocessable_entity
 end
 end
+
+def update
+@user = User.find(params[:id])
+if @user.update(params.require(:user).permit(:username, :email, :password))
+flash[:notice] = "your account has been updated successfully!"
+redirect_to articles_path
+else
+render :edit, status: :unprocessable_entity
+end
+end
+
 end
